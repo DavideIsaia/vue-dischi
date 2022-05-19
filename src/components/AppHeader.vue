9fdd239f58a154e8b8c6d07f750ca6103cbb1246
DavideIsaia <davide.enjoy@hotmail.it>
2022-05-19 14:38:22 +0200

    aggiunto header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <a href="https://www.spotify.com/">
+      <img src="../assets/spotify-download-logo-30.png" alt="Spotify logo" />
+    </a>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  background-color: #2e3a46;
+
+  img {
+    width: 80px;
+    margin: 0.5rem 2rem;
+  }
+}
+</style>
