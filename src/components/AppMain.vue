12af6af057b0aa5719a052c60645bcf358cf70ac
DavideIsaia <davide.enjoy@hotmail.it>
2022-05-19 15:16:33 +0200

    aggiunta card statica

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,8 +1,21 @@
-<template><main></main></template>
+<template>
+  <main class="container">
+    <div class="row d-flex flex-wrap">
+      <div class="col-2 mt-5">
+        <DiscCard />
+      </div>
+    </div>
+  </main>
+</template>
 
 <script>
+import DiscCard from "./DiscCard.vue";
+
 export default {
   name: "AppMain",
+  components: {
+    DiscCard,
+  },
 };
 </script>
 
---
+++ src/components/DiscCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="my-card text-center">
+    <img
+      src="https:\/\/www.onstageweb.com\/wp-content\/uploads\/2018\/09\/bon-jovi-new-jersey.jpg"
+      alt=""
+    />
+    <h2>Titolo</h2>
+    <h3>Artista</h3>
+    <h3>Anno</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DiscCard",
+};
+</script>
+
+<style>
+.my-card {
+  background-color: #2e3a46;
+}
+</style>
